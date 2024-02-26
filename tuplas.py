0dbf196216835c3edec268479e1e947bd2a3bdb3
himayrob <160776152+himayrob@users.noreply.github.com>
2024-02-26 09:21:47 -0500

    ahi vamos metiendole logica}

---
+++ tuplas.py
@@ -0,0 +1,21 @@
+direccion = input ("dame las direcciones, escriba x si no llena los csmpod")
+input ("dame las direcciones")}
+input ("dame las direcciones")
+input ("dame las direcciones")
+imformacion = input ("dame imformacion, escriba x si no llena los campos")
+input ("dame imformacion")
+input ("dame imformacion")
+input ("dame imformacion")
+cursos =input ("escribe los cursos, escriba x si no llena los campos")
+input ("escribe los cursos")
+input ("escribe los cursos")
+input ("escribe los cursos")
+imformacion laboral =input ("dame tu imforcaion laboral")
+input ("dame tu imforcaion laboral")
+input ("dame tu imforcaion laboral")
+input ("dame tu imforcaion laboral
+")
+tuple:print direccion
+tuple:print imformacion
+tuple:print cursos
+tuple:print imformacion laboral
